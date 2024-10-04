e2905068712883da3605838f34f2f0b77b2a100e
LeoRubio05 <lfajardor05@hotmail.com>
2024-10-03 22:39:41 -0700

    Unit 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://stunning-longma-29c3f8.netlify.app/"
+  site: "https://stunning-longma-29c3f8.netlify.app/",
+  integrations: [preact()]
 });
