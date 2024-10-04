f03dd17ec21fc51662be2a84bdf7517f4cf1601e
LeoRubio05 <lfajardor05@hotmail.com>
2024-10-03 22:03:10 -0700

    Unit 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://stunning-longma-29c3f8.netlify.app/"
+});
